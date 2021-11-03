ec8261408bb02bc05060d0dfaccbc1e0e4a620ec
Andrés Morales <mora200217@hotmail.com>
2021-11-02 21:50:23 -0500

    Sine 3d effect

--- sketch.js
+++ sketch.js
@@ -3,22 +3,32 @@
 const W = innerWidth; 
 const H = innerHeight; 
 
+let t = 0; 
+
 function setup(){
     createCanvas(W, H); 
 }
 function draw(){
     background(0); 
     
-    const step = 0.2; 
-    const RAD = 20; 
+    
+    
+    const RAD = 2; 
+    const step = 2 * RAD + 1; 
 
     for (let x = 0; x < W; x += step){
-        circle(x, getPosY(x), RAD); 
+        circle(x, getPosY(x, t), RAD); 
     }   
+
+    t++; 
 }
 
-function getPosY(posX){
-   
-    return 10; 
-}
+function getPosY(posX, t){
+    const A = 100; 
+    const w = 0.01; 
+
+    
+    let y = A * sin(t * w + posX ) + H/2; 
+     return y; 
+ }
 
